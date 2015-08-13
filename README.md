e476569af63167831168bc8e8676c2889ad5a947
sen.cenan@gmail.com <sen.cenan@gmail.com>
2015-08-12 21:34:26 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Docker image for riak. Based off Java:8 since riak search is solr based.
---
+++ bin/riak-setup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+IP_ADDRESS=$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
+
+# Ensure correct ownership and permissions on volumes
+chown -R riak:riak /var/lib/riak /var/log/riak /etc/riak/riak.conf
+chmod -R 755 /var/lib/riak /var/log/riak /etc/riak/riak.conf
+
+# Increase open file descriptor limit
+ulimit -n 65536
+
+# Ensure the Erlang node name is set correctly
+sed -i.bak "s/riak@127.0.0.1/riak@${IP_ADDRESS}/" /etc/riak/riak.conf
+
+# rm old ring files before starting
+rm -rf /var/lib/riak/ring/*
+
+# Start supervisord
+exec /usr/bin/supervisord
